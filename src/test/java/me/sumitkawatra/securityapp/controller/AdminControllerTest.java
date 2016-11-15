60d3dc1b2692bebc7eb52c5d27d33b4b49207dc3
Sumit Kawatra <sumitkkawatra@gmail.com>
2016-11-15 23:55:11 +0530

    basic http security using spring-security - 4.2.0

---
+++ src/test/java/me/sumitkawatra/securityapp/controller/AdminControllerTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package me.sumitkawatra.securityapp.controller;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sumitkawatra
+ *
+ */
+public class AdminControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//TODO
+	}
+
+}
